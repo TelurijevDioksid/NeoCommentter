d66ae21bcd7cf233ed28d4480b09ba217de082e6
Teo Radolovict <tr54427@fer.hr>
2024-04-19 23:18:29 +0200

    v1.0.0

--- lua/nvim-commenter/init.lua
+++ lua/nvim-commenter/init.lua
@@ -2,44 +2,20 @@ local M = {}
 local utils = require("nvim-commenter.utils")
 local langs = require("nvim-commenter.langs")
 
-function M.find_line_pos()
-    local start_num = 0
-    local end_num = 0
-    local mode = vim.api.nvim_get_mode().mode
-
-    if mode == "n" then
-        end_num, _ = unpack(vim.api.nvim_win_get_cursor(0))
-        start_num = end_num - 1
-    else
-        start_num = vim.fn.getpos("'<")[2]
-        end_num = vim.fn.getpos("'>")[2]
-    end
-    return start_num, end_num
-end
-
 function M.setup(props)
     if props then
         langs.add_langs(props)
     end
 end
 
-function M.toggle_comment(start_arg, end_arg)
-    local start_num, end_num = M.find_line_pos()
-    if start_arg and end_arg then
-        start_num = start_arg
-        end_num = end_arg
-    end
+function M.toggle_comment()
+    local start_num, end_num = utils.find_line_pos()
     local lines = vim.api.nvim_buf_get_lines(0, start_num, end_num, false)
     local info = utils.get_f_buff_lang()
     local commented = false
 
-    print(start_num, end_num)
-    for _, l in ipairs(lines) do
-        print(l)
-    end
-
     for _, line in ipairs(lines) do
-        local start_match = string.match(line, "^" .. utils.insert_char_before(info[1]) .. " ")
+        local start_match = string.match(line, "^%s*" .. utils.insert_char_before(info[1]) .. " ")
         if start_match then
             commented = true
             break
@@ -47,24 +23,24 @@ function M.toggle_comment(start_arg, end_arg)
     end
 
     if commented then
-        utils.uncomment_lines(start_num, lines, info)
+        utils.uncomment_lines(start_num, end_num, lines, info)
     else
-        utils.comment_lines(start_num, lines, info)
+        utils.comment_lines(start_num, end_num, lines, info)
     end
 end
 
 function M.uncomment()
     local start_num, end_num = utils.find_line_pos()
     local lines = vim.api.nvim_buf_get_lines(0, start_num, end_num, false)
-    utils.uncomment_lines(start_num, end_num, lines)
+    local info = utils.get_f_buff_lang()
+    utils.uncomment_lines(start_num, end_num, lines, info)
 end
 
 function M.comment()
     local start_num, end_num = utils.find_line_pos()
+    local info = utils.get_f_buff_lang()
     local lines = vim.api.nvim_buf_get_lines(0, start_num, end_num, false)
-    utils.comment_lines(start_num, end_num, f_buff)
+    utils.comment_lines(start_num, end_num, lines, info)
 end
-
-M.setup()
 
 return M
--- lua/nvim-commenter/utils.lua
+++ lua/nvim-commenter/utils.lua
@@ -9,55 +9,69 @@ function M.insert_char_before(str)
     return res
 end
 
-function M.comment_lines(start_idx, lines, info)
+function M.comment_lines(start_idx, end_idx, lines, info)
     local new_lines = {}
 
     for _, line in ipairs(lines) do
-        local new_l = info[1] .. " " .. line
-        if info[2] ~= "" then
-            new_l = new_l .. " " .. info[2]
+        local line_whitespace = string.match(line, "^%s*")
+        local line_text = string.match(line, "^%s*(%S.*)")
+        if line_text and line_whitespace then
+            line = line_whitespace .. info[1] .. " " .. line_text
+            if info[2] ~= "" then
+                line = line .. " " .. info[2]
+            end
         end
-        table.insert(new_lines, new_l)
+        table.insert(new_lines, line)
     end
 
-    for _, line in ipairs(new_lines) do
-        print(line)
-    end
-    vim.api.nvim_buf_set_lines(0, start_idx, start_idx, false, new_lines)
+    vim.api.nvim_buf_set_lines(0, start_idx, end_idx, false, new_lines)
 end
 
-function M.uncomment_lines(line_idx, lines, info)
+function M.uncomment_lines(start_idx, end_idx, lines, info)
     local new_lines = {}
 
     for _, line in ipairs(lines) do
+        local start_whitespace = string.match(line, "^%s*")
         local start_match = string.match(line, "^%s*" .. M.insert_char_before(info[1]) .. "%s*")
         local end_match = string.match(line, "%s*" .. M.insert_char_before(info[2]) .. "$s*$")
-        local new_l = line
+        local pos = nil
         if start_match then
-            new_l = string.gsub(new_l, start_match, "", 1)
+            print("start_match", start_match)
+            print("line", line)
+            _, pos = string.find(line, start_match, 1, true)
+            print("pos", pos)
+            line = string.sub(line, pos + 1)
         end
         if end_match then
-            new_l = string.gsub(new_l, end_match, "", 1)
+            pos, _ = string.find(line, end_match, 1, true)
+            line = string.sub(line, 1, pos - 1)
         end
-        table.insert(new_lines, new_l)
+        line = start_whitespace .. line
+        table.insert(new_lines, line)
     end
 
-    vim.api.nvim_buf_set_lines(0, start_idx, start_idx, false, new_lines)
+    vim.api.nvim_buf_set_lines(0, start_idx, end_idx, false, new_lines)
 end
 
 function M.find_line_pos()
     local start_num = 0
     local end_num = 0
-    local mode = vim.api.nvim_get_mode().mode
 
-    if mode == "n" then
-        end_num, _ = unpack(vim.api.nvim_win_get_cursor(0))
+    if vim.api.nvim_get_mode().mode == "n" then
+        end_num = vim.api.nvim_win_get_cursor(0)[1]
         start_num = end_num - 1
     else
-        start_num = vim.fn.getpos("'<")[2]
-        end_num = vim.fn.getpos("'>")[2]
+        start_num = vim.fn.getpos("v")[2]
+        end_num = vim.fn.getpos(".")[2]
+        local help = 0
+        if start_num > end_num then
+            help = start_num
+            start_num = end_num - 1
+            end_num = help
+        else
+            start_num = start_num - 1
+        end
     end
-    print(start_num, end_num)
     return start_num, end_num
 end
 
