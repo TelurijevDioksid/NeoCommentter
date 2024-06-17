ee8c8d631e0394fd426153a1deacedbfe170e21e
Teo Radolovict <tr54427@fer.hr>
2024-06-17 22:16:01 +0200

    if comment not found take '//'

--- lua/NeoCommentter/init.lua
+++ lua/NeoCommentter/init.lua
@@ -7,6 +7,7 @@ local function setup(props)
         file:write("{}")
         file:close()
     end
+
     if props["langs"] then
         langs.add_langs(props["langs"])
     end
@@ -16,9 +17,11 @@ local function toggle_comment()
     local start_idx, end_idx = utils.get_selection()
     local lines = vim.api.nvim_buf_get_lines(0, start_idx, end_idx, false)
     local info = langs.get_lang_info()
+
     if not info then
-        error("Cannot find comment for this language. Check your setup.")
+        info = { begin = "//", last = "" }
     end
+
     local new_lines = {}
     if utils.has_comment(lines, info.begin) then
         new_lines = utils.remove_comment(info.begin, info.last, lines)
@@ -32,8 +35,9 @@ local function uncomment()
     local start_idx, end_idx = utils.get_selection()
     local lines = vim.api.nvim_buf_get_lines(0, start_idx, end_idx, false)
     local info = langs.get_lang_info()
+
     if not info then
-        error("Cannot find comment for this language. Check your setup.")
+        info = { begin = "//", last = "" }
     end
     local new_lines = utils.remove_comment(info.begin, info.last, lines)
     vim.api.nvim_buf_set_lines(0, start_idx, end_idx, false, new_lines)
@@ -43,8 +47,9 @@ local function comment()
     local start_idx, end_idx = utils.get_selection()
     local lines = vim.api.nvim_buf_get_lines(0, start_idx, end_idx, false)
     local info = langs.get_lang_info()
+
     if not info then
-        error("Cannot find comment for this language. Check your setup.")
+        info = { begin = "//", last = "" }
     end
     local new_lines = utils.add_comment(info.begin, info.last, lines)
     vim.api.nvim_buf_set_lines(0, start_idx, end_idx, false, new_lines)
